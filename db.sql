09473b6a50f8af8e39eaadeec3e9c894b7763fc4
stowka <antoine@netproduction.fr>
2013-12-22 19:11:22 +0100

    db - create table - user, message

--- db.sql
+++ db.sql
@@ -11,8 +11,16 @@ create table `user` (
 	`id` bigint(20) not null auto_increment,
 	`username` varchar(64) not null,
 	`email` varchar(64) not null,
-	
+	`picture` tinyint(1) not null,
 	primary key(`id`),
 	unique (`username`),
 	unique (`email`)
 ) engine=InnoDB auto_increment=500 default charset=`utf-8`;
+
+create table `message` (
+	`ìd` bigint(10) not null auto_increment,
+	`from` bigint(10) not null,
+	`to` bigint(10) not null,
+	`text` varchar(256)  not null,
+	primary key(`id`)
+) engine=InnoDB auto_increment=1 default charset=`utf-8`;
