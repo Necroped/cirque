eacf61137ee4412e74e81f3fa93eecdac8c3953e
stowka <antoine@netproduction.fr>
2013-12-23 01:48:09 +0100

    add rate, comment - lock before insert - unsigned id - comments

--- db.sql
+++ db.sql
@@ -61,67 +61,67 @@ use cirque;
 --
 
 create table `circus` (
-	`id` bigint(10) not null auto_increment,
+	`id` bigint(10) unsigned not null auto_increment,
 	`name` varchar(32) not null,
-	`country` smallint(2) default 0,
-	`description` varchar(1024) default '',
-	`picture` tinyint(1) default 0,
+	`country` smallint(2) unsigned not null default 0,
+	`description` varchar(1024) not null default '',
+	`picture` tinyint(1) unsigned not null default 0,
 	primary key (`id`),
 	unique key (`name`)
 ) engine=InnoDB auto_increment=500 default charset=`utf8`;
 
 create table `city` (
-	`id` bigint(10) not null auto_increment,
+	`id` bigint(10) unsigned not null auto_increment,
 	`name` varchar(32) not null,
-	`country` bigint(10) not null,
-	`latitude` decimal(10, 8) not null,
-	`longitude` decimal(11, 8) not null,
+	`country` bigint(10) unsigned not null,
+	`latitude` decimal(10, 8) not null comment 'Between -90 and 90 degrees',
+	`longitude` decimal(11, 8) not null comment 'Between -180 and 180 degrees',
 	primary key (`id`)
 ) engine=InnoDB auto_increment=1 default charset=`utf8`;
 
 create table `country` (
-	`id` smallint(2) not null auto_increment,
+	`id` smallint(2) unsigned not null auto_increment,
 	`name` varchar(16) not null,
-	`latitude` decimal(10, 8) not null,
-	`longitude` decimal(11, 8) not null,
+	`latitude` decimal(10, 8) not null comment 'Between -90 and 90 degrees',
+	`longitude` decimal(11, 8) not null comment 'Between -180 and 180 degrees',
 	primary key (`id`)
 ) engine= InnoDB auto_increment=1 default charset=`utf8`;
 
 create table `event` (
-	`id` bigint(10) not null auto_increment,
+	`id` bigint(10) unsigned not null auto_increment,
 	`from` date not null,
 	`to` date not null,
-	`city` bigint(10) default 0,
-	`description` varchar(128) default 'No description',
-	`circus` bigint(10) not null,
+	`city` bigint(10) unsigned not null default 0,
+	`description` varchar(128) not null default 'No description',
+	`circus` bigint(10) unsigned not null,
 	primary key (`id`)
 ) engine= InnoDB auto_increment=500 default charset=`utf8`;
 
 create table `message` (
-	`id` bigint(10) not null auto_increment,
-	`from` bigint(10) not null,
-	`to` bigint(10) not null,
-	`text` varchar(256)  not null,
-	`datetime` datetime default current_timestamp,
+	`id` bigint(10) unsigned not null auto_increment,
+	`from` bigint(10) unsigned not null,
+	`to` bigint(10) unsigned not null,
+	`text` varchar(256) not null,
+	`datetime` datetime not null default current_timestamp,
 	primary key (`id`)
 ) engine=InnoDB auto_increment=1 default charset=`utf8`;
 
 create table `picture` (
-	`id` bigint(10) not null auto_increment,
-	`date` timestamp default current_timestamp on update current_timestamp,
-	`event` bigint(10) not null,
-	`description` varchar(64) default 'No description',
+	`id` bigint(10) unsigned not null auto_increment,
+	`date` datetime not null default current_timestamp on update current_timestamp,
+	`event` bigint(10) unsigned not null,
+	`description` varchar(64) not null default 'No description',
 	primary key (`id`)
 ) engine=InnoDB auto_increment=500 default charset=`utf8`;
 
 create table `user` (
-	`id` bigint(20) not null auto_increment,
+	`id` bigint(20) unsigned not null auto_increment,
 	`username` varchar(64) not null,
 	`password` varchar(40) not null,
 	`firstName` varchar(16) not null,
 	`lastName` varchar(16) not null,
 	`email` varchar(64) not null,
-	`picture` tinyint(1) default 0,
+	`picture` tinyint(1) unsigned not null default 0,
 	primary key (`id`),
 	unique key (`username`),
 	unique key (`email`)
@@ -130,34 +130,56 @@ create table `user` (
 --
 -- Association tables
 --
+create table `comment`(
+	`user` bigint(10) unsigned not null,
+	`picture` bigint(10) unsigned not null,
+	`text` varchar(256) not null,
+	primary key (`user`, `picture`)
+) engine=InnoDB default charset=`utf8`;
 
 create table `followCircus` (
-	`user` bigint(10) not null,
-	`circus` bigint(10) not null,
-	`datetime` datetime default current_timestamp,
+	`user` bigint(10) unsigned not null,
+	`circus` bigint(10) unsigned not null,
+	`datetime` datetime not null default current_timestamp,
 	primary key (`user`, `circus`)
 ) engine=InnoDB default charset=`utf8`;
 
 create table `followUser` (
-	`following` bigint(10) not null,
-	`follower` bigint(10) not null,
-	`datetime` datetime default current_timestamp,
+	`following` bigint(10) unsigned not null,
+	`follower` bigint(10) unsigned not null,
+	`datetime` datetime not null default current_timestamp,
 	primary key (`following`, `follower`)
+) engine=InnoDB default charset=`utf8`;
+
+create table `rate` (
+	`user` bigint(10) unsigned not null,
+	`picture` bigint(10) unsigned not null,
+	`date` datetime not null default current_timestamp on update current_timestamp,
+	`grade` tinyint(1) unsigned not null comment 'Between 0 and 10',
+	primary key (`user`, `picture`)
 ) engine=InnoDB default charset=`utf8`;
 
 --
 -- Data
 --
 
+lock tables `circus` write;
 insert into circus (name) 
 values ("L'Odyssée du Cirque"), ("Cirque Pinder"), ("Cirque du Soleil");
+unlock tables;
 
+lock tables `city` write;
 insert into city (name, country, latitude, longitude)
 values ("Belfort", 1, 47.37590000, 6.52000000);
+unlock tables;
 
+lock tables `country` write;
 insert into country (id, name, latitude, longitude)
 values (1, "France", 46.00000000, 2.00000000);
+unlock tables;
 
+lock tables `user` write;
 insert into user (username, password, firstName, lastName, email)
 values ("stowka", sha1("893QQY"), "Antoine", "De Gieter",
 "antoine@netproduction.fr");
+unlock tables;
