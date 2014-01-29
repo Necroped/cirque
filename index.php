7158fba383b3251c55c298a2f0e50964fe3195d6
stowka <antoine@netproduction.fr>
2014-01-29 10:13:24 +0100

    home page

--- index.php
+++ index.php
@@ -4,4 +4,8 @@
 	 * @author Antoine De Gieter
 	 */
 
-	include_once "config/congig.inc";
+	include_once "config/config.inc";
+
+	if ( isset( $_GET['page'] ) && in_array( $_GET['page'], $allowedPages ) ):
+		include_once "controllers/" . $_GET['page'] . ".php";
+	endif;
