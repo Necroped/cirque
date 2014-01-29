611aa1a304317b9b73166c55d4f546353040e292
stowka <antoine@netproduction.fr>
2014-01-29 10:13:54 +0100

    home page

---
+++ views/home.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Home</title>
+
+		<link rel="stylesheet" type="text/css" href="global/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="global/css/admin-theme.css">
+	</head>
+
+	<body>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+					
+				</div>
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+					<div class="padded"></div>
+					<ul class="nav nav-pills nav-stacked">
+						<li class="text-center active">
+							<a href="#">
+								Home
+							</a>
+						</li>
+
+						<li class="text-center">
+							<a href="#">
+								Valider des photos
+							</a>
+						</li>
+
+						<li class="text-center">
+							<a href="#">
+								Toutes les photos
+							</a>
+						</li>
+
+						<li class="text-center">
+							<a href="#">
+								Tous les cirques
+							</a>
+						</li>
+
+						<li class="text-center">
+							<a href="#">
+								Tous les utilisateurs
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+					
+				</div>
+			</div>
+		</div>
+
+		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+		<script type="text/javascript" src="global/js/bootstrap.min.js"></script>
+	</body>
+</html>
