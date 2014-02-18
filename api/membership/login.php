4255694056957864c028e355030623eab37788ab
stowka <antoine@netproduction.fr>
2014-02-18 17:16:59 +0100

    fix login

--- api/membership/login.php
+++ api/membership/login.php
@@ -6,19 +6,16 @@
 	&& isset( $_GET['key'] )
 	&& $_GET['key'] === sha1( "odyssee" ) ):
 		$dbh = SPDO::getInstance();
-		$stmt = $dbh->prepare( "SELECT count(*) FROM user 
+		$stmt = $dbh->prepare( "SELECT id FROM user 
 			WHERE username = :username AND password = sha1(:password);" );
 		$stmt->bindParam( "username", $_GET['username'], PDO::PARAM_STR );
 		$stmt->bindParam( "password", $_GET['password'], PDO::PARAM_STR );
 		$stmt->execute();
-		if ( $connect = $stmt->fetch( PDO::FETCH_NUM ) ):
+		if ( $id = $stmt->fetch( PDO::FETCH_NUM ) ):
 			$response = array( "error" => false );
-			if ( !$connect[0] )
-				$response["connected"] = false;
-			else
-				$response["connected"] = true;
+			$response["connected"] = $id[0];
 		else:
-			$response = array( "error" => true, "stack_trace" => "unexpected error" );
+			$response = array( "error" => true, "stack_trace" => "wrong username or password" );
 		endif;
 		$stmt->closeCursor();
 	else:
