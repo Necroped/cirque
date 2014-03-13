96c41cb12bd86459541fa10bd74d72cc762f1186
stowka <antoine@netproduction.fr>
2014-03-13 16:56:19 +0100

    update last events

--- api/activity/last_events.php
+++ api/activity/last_events.php
@@ -3,6 +3,9 @@
 	require_once "../../lib/spdo.class.php";
 	require_once "../../lib/functions.php";
 
+	$_GET['circus'] = 1;
+	$_GET['key'] = sha1("odyssee");
+
 	if ( isset( $_GET['circus'] ) && is_numeric( $_GET['circus'] )
 	&& isset( $_GET['key'] )
 	&& $_GET['key'] === sha1( "odyssee" ) ):
@@ -11,10 +14,10 @@
 		$stmt->bindParam( ":circus", $_GET['circus'], PDO::PARAM_INT );
 		$stmt->execute();
 		$response = array( "error" => false );
-		while ( $event = $stmt->fetch( PDO::FETCH_ASSOC ) ):
-			$response = array_merge( $response, $event );
-			array_walk_recursive( $response, 'conv' );
-		endwhile;
+		$events = array();
+		$events['events'] = $stmt->fetchAll( PDO::FETCH_ASSOC );
+		$response = array_merge( $response, $events );
+		array_walk_recursive( $response, 'conv' );
 		$stmt->closeCursor();
 	else:
 		$response = array( "error" => true, "stack_trace" => "id error" );
--- api/location/map_markers.php
+++ api/location/map_markers.php
@@ -4,7 +4,7 @@
 	require_once "../../lib/functions.php";
 
 	$dbh = SPDO::getInstance();
-	$stmt = $dbh->prepare( "SELECT ci.name as circus, e.fromDate as date, c.latitude, c.longitude FROM city c INNER JOIN event e ON e.city = c.id INNER JOIN circus ci ON e.circus = ci.id WHERE e.fromDate > NOW() GROUP BY e.city;" );
+	$stmt = $dbh->prepare( "SELECT ci.name as circus, e.fromDate as date, c.latitude, c.longitude as pictures FROM city c INNER JOIN event e ON e.city = c.id INNER JOIN circus ci ON e.circus = ci.id WHERE e.fromDate > NOW() GROUP BY e.city;" );
 	$stmt->execute();
 	if ( $cities = $stmt->fetchAll( PDO::FETCH_ASSOC ) ):
 		$response = array( "error" => false );
