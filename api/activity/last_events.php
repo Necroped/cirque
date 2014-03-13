9770937597778d60dd2ed732f965a1ea0acee6cf
stowka <antoine@netproduction.fr>
2014-03-13 16:57:50 +0100

    update again

--- api/activity/last_events.php
+++ api/activity/last_events.php
@@ -3,14 +3,11 @@
 	require_once "../../lib/spdo.class.php";
 	require_once "../../lib/functions.php";
 
-	$_GET['circus'] = 1;
-	$_GET['key'] = sha1("odyssee");
-
 	if ( isset( $_GET['circus'] ) && is_numeric( $_GET['circus'] )
 	&& isset( $_GET['key'] )
 	&& $_GET['key'] === sha1( "odyssee" ) ):
 		$dbh = SPDO::getInstance();
-		$stmt = $dbh->prepare( "SELECT * FROM event WHERE circus = :circus ORDER BY fromDate DESC;" );
+		$stmt = $dbh->prepare( "SELECT id FROM event WHERE circus = :circus ORDER BY fromDate DESC;" );
 		$stmt->bindParam( ":circus", $_GET['circus'], PDO::PARAM_INT );
 		$stmt->execute();
 		$response = array( "error" => false );
